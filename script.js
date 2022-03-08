c0f0e1eee3b07373cc6ae7963a82051107978ba0
Swareeh <77015406+Swareeh@users.noreply.github.com>
2022-03-08 12:30:48 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+// Tryouts form
+(function () { var r, d = document, gt = d.getElementById, cr = d.createElement, tg = d.getElementsByTagName, id = "aidaform-embed"; if (!gt.call(d, id)) { r = cr.call(d, "script"); r.id = id; r.src = "https://embed.aidaform.com/embed.js"; (d.head || tg.call(d, "head")[0]).appendChild(r); } })()
+
+// Toggle Button
+
+const toggleButton = document.getElementsByClassName('toggle-btn')[0]
+const navbarLinks = document.getElementsByClassName('nav_links')[0]
+
+toggleButton.addEventListener('click', () => {
+  navbarLinks.classList.toggle('active')
+})

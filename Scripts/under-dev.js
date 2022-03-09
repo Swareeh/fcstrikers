2908811f85a9a9e040ef3721213d1297b940dfa4
Swareeh <77015406+Swareeh@users.noreply.github.com>
2022-03-09 17:21:33 +0400

    Add files via upload

---
+++ Scripts/under-dev.js
@@ -0,0 +1,17 @@
+// Nav Bar 
+
+function toggleBtn() {
+
+    const navLinks = document.getElementById('nav_links')
+    const contactBtn = document.getElementById('contact-btn')
+
+    if (navLinks.style.display === "none") {
+        navLinks.style.display = "block";
+        contactBtn.style.display = "block";
+    } else {
+        navLinks.style.display = "none";
+        contactBtn.style.display = "none";
+
+    }
+
+}

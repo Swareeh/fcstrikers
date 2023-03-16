6f075e28f066a62b104c45d8469691f741cb46f0
Swareeh <77015406+Swareeh@users.noreply.github.com>
2023-03-16 11:29:58 +0400

    Add files via upload

---
+++ Scripts/contact.js
@@ -0,0 +1,22 @@
+// Nav Bar 
+
+toggleBtn()
+
+function toggleBtn() {
+
+    const navLinks = document.getElementById('nav_connections')
+    const web_body = document.getElementById('web_body')
+
+    if (navLinks.style.right === "450px") {
+        navLinks.style.right = "0px";
+        web_body.style.overflow = "hidden";
+    } else {
+        navLinks.style.right = "450px";
+        web_body.style.overflow = "visible";
+    }
+
+}
+
+// Form
+(function () { var r, d = document, gt = d.getElementById, cr = d.createElement, tg = d.getElementsByTagName, id = "aidaform-embed"; if (!gt.call(d, id)) { r = cr.call(d, "script"); r.id = id; r.src = "https://embed.aidaform.com/embed.js"; (d.head || tg.call(d, "head")[0]).appendChild(r); } })()
+
